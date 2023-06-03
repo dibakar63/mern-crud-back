65fe5c073797c8d06890d6872e0722defb799d81
Dibakar Dey <110836157+dibakar63@users.noreply.github.com>
2023-06-03 22:17:36 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,11 +1,11 @@
 const mongoose=require('mongoose');
-const connectDB=async()=>{
-try {
-    let connect=await mongoose.connect('mongodb+srv://dibakardey63:1r28OwcgbtFwpJXN@recipes.rglhoml.mongodb.net/tournamnet');
-    console.log(`mongoose connected `)
-} catch (error) {
-   console.log(error) 
-}
-}
 
-module.exports=connectDB;
+  const connectDB=async()=>{
+    try {
+        await mongoose.connect('mongodb+srv://dibakardey63:S8oNncTxdBeqntKE@recipes.rglhoml.mongodb.net/tournamnet');
+        console.log("db connected")
+    } catch (error) {
+        console.log(error)
+    }
+ }
+ module.exports=connectDB
